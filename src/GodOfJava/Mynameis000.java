683a5a1c44ff9997ef4bb4d70461cc15defce696
SungWook-csw <choisw950928@naver.com>
2021-03-11 00:44:52 +0900

    feat: GodOfJava1nd / Chapter2

---
+++ src/GodOfJava/Mynameis000.java
@@ -0,0 +1,7 @@
+package GodOfJava;
+
+public class Mynameis000 {
+    public static void main(String[] args) {
+        System.out.println("My name is SungWookChoi");
+    }
+}
---
+++ src/GodOfJava/Profile.java
@@ -0,0 +1,10 @@
+package GodOfJava;
+
+public class Profile {
+    public static void main(String[] args) {
+        String name = "최성욱"; //문자열
+        int age = 27; // int = 변수
+        System.out.println("My name is "+name);
+        System.out.println("My age is "+age);
+    }
+}
