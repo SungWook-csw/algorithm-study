9698597b0d8461372419d4d40687d73f241e7bbb
SungWook-csw <choisw950928@naver.com>
2021-03-14 23:56:01 +0900

    feat: 생활코딩 6.3 Number_Operation

---
+++ src/LifeCoding/Number_Operation.java
@@ -0,0 +1,14 @@
+package LifeCoding;
+
+public class Number_Operation { // 숫자와 연산
+    public static void main (String [] args){
+        System.out.println(6 + 2); // 8
+        System.out.println(6 - 2); // 4
+        System.out.println(6 * 2); // 12
+        System.out.println(6 / 2); // 3
+
+        System.out.println(Math.PI); // 원주율 3.1415926....
+        System.out.println(Math.floor(Math.PI)); // 반내림 3.0
+        System.out.println(Math.ceil(Math.PI)); // 반올림 4.0
+    }
+}
