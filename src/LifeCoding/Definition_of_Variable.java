38d743c632edaa57ce393f9476a347aecd3a2d14
SungWook-csw <choisw950928@naver.com>
2021-03-15 00:46:14 +0900

    feat: 생활코딩 8 Definition_of_Variable

---
+++ src/LifeCoding/Definition_of_Variable.java
@@ -0,0 +1,16 @@
+package LifeCoding;
+
+public class Definition_of_Variable {
+    public static void main(String[] args) {
+
+// integer => 정수
+        int a = 1; // Number -> integer ... -2, -1, 0, 1, 2 ...
+        System.out.println(a);
+// double => 실수만 사용할 수 있음
+        double b = 1.1; // real number -> double .. -2.0,-1.0, 0, 1.0, 2.0..
+        System.out.println(b);
+// String => 문자열
+        String c = "Hello World";
+        System.out.println(c);
+    }
+}
