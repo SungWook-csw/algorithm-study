260c9c9acf5072b6fde7faa23bb308c0d497715f
SungWook-csw <choisw950928@naver.com>
2021-03-15 00:27:37 +0900

    feat: 생활코딩 6.5 Handing_Strings2

---
+++ src/LifeCoding/Handing_Strings2.java
@@ -0,0 +1,9 @@
+package LifeCoding;
+
+public class Handing_Strings2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World".length());
+        System.out.println("Hello, [[[최성욱]]] ... bye. ".replace("[[[최성욱]]]", "egoing"));
+    }
+}
