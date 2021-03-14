b804f9dbc45ff339e931c9f40c8c2c63ae4cb685
SungWook-csw <choisw950928@naver.com>
2021-03-14 23:44:15 +0900

    feat: 생활코딩 6.1 Data_and_Operation

---
+++ src/LifeCoding/Data_and_operation.java
@@ -0,0 +1,24 @@
+package LifeCoding;
+
+public class Data_and_operation {
+    public static void main(String [] args) {
+        System.out.println(6); // 숫자를 표현 할 때
+        System.out.println("Six"); // String "문자" 정확하게는 문자열
+        System.out.println("6"); // String 문자 6
+
+        System.out.println(6+6); // 12
+        System.out.println("6"+"6"); // 66
+
+        System.out.println(6*6); // 36
+//      System.out.println("6"*"6");
+
+        System.out.println("1111".length()); // 4 문자열의 길이를 알려줌
+        System.out.println(1111); // 1111
+//      System.out.println(1111.length());
+
+        System.out.println("Hello World"); // String 문자열
+        System.out.println('H'); // Char 문자
+        System.out.println("H");
+
+    }
+}
