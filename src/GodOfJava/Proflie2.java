5f006e310cc33390de7cc7d7be10fdbc60137755
SungWook-csw <choisw950928@naver.com>
2021-03-17 22:40:19 +0900

    feat: 생활코딩 13.3 How to view Java documentation

---
+++ src/GodOfJava/Proflie2.java
@@ -0,0 +1,9 @@
+package GodOfJava;
+
+public class Proflie2 {
+    public static void main (String [] args) {
+        System.out.println();
+
+
+    }
+}
--- src/LifeCoding/Handing_Strings2.java
+++ src/LifeCoding/Handing_Strings2.java
@@ -2,7 +2,7 @@ package LifeCoding;
 
 public class Handing_Strings2 {
     public static void main(String[] args) {
-
+// replace => 바꾸다
         System.out.println("Hello World".length());
         System.out.println("Hello, [[[최성욱]]] ... bye. ".replace("[[[최성욱]]]", "egoing"));
     }
