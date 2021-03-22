f9d1ea6e2ff2342f1102d15f10ba2293768e1fb0
SungWook-csw <choisw950928@naver.com>
2021-03-22 21:46:33 +0900

    Chapter3. CarManager

--- src/GodOfJava/Calculator.java
+++ src/GodOfJava/Calculator.java
@@ -2,7 +2,23 @@ package GodOfJava;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Calculator class started");
-
+    }
+    public int add(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+    public int addAll(int num1, int num2){
+        int result = num1 - num2;
+        return result;
+    }
+    public int multiply(int num1, int num2){
+        int result;
+        result = num1 * num2;
+        return result;
+    }
+    public int divide(int num1, int num2){
+        int result = num1 / num2;
+        return result;
     }
 }
