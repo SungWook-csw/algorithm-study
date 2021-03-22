ce5d8b07843e2aae4fd86b4a1dc9ebe4167bf99a
SungWook-csw <choisw950928@naver.com>
2021-03-22 23:30:38 +0900

    feat : 백준 Q1008

---
+++ src/BAEkJOON/Q1008.java
@@ -0,0 +1,15 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+public class Q1008 {
+    public static void main (String [] args){
+        Scanner sacn = new Scanner(System.in);
+
+        double a = sacn.nextDouble();
+        double b = sacn.nextDouble();
+
+        System.out.println( a / b);
+
+    }
+
+}
