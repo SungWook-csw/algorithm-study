94296d3eb3467f968e1170f2763c47101c9a4db5
SungWook-csw <choisw950928@naver.com>
2021-03-25 23:00:20 +0900

    feat : 생활코딩 비교연산자

---
+++ src/LifeCoding/ComparisonOperator.java
@@ -0,0 +1,11 @@
+package LifeCoding;
+
+public class ComparisonOperator {
+    public static void main(String[] args) {
+
+        System.out.println(1 > 1); // false 거짓
+        System.out.println(1 == 1); // true 진실
+        System.out.println(1 < 1);
+        System.out.println(1 >= 1);
+    }
+}
