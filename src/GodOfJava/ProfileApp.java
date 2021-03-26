a410542e5821299f43fc8696c6e41a5e38b726bf
SungWook-csw <choisw950928@naver.com>
2021-03-27 01:23:29 +0900

    feat : GodOfJava 1nd Chap.3

--- src/GodOfJava/ProfileApp.java
+++ src/GodOfJava/ProfileApp.java
@@ -1,11 +1,28 @@
 package GodOfJava;
 
 public class ProfileApp {
-    public static void main(String[] args) {
-        String name = "최성욱";
-        int age = 27;
+    String name = "OOO";
+    int age = 0;
 
-        System.out.println("My name is" + "최성욱");
-        System.out.println("My is age" + 27);
+    public static void main(String[] args) {
+        ProfileApp profile=new ProfileApp();
+        profile.setName("Min");
+        profile.setAge(20);
+        profile.printName();
+        profile.printAge();
+    }
+
+    public void setName(String str) {
+        name = str;
+    }
+    public void setAge(int value) {
+        age = value;
+    }
+    public void printName() {
+        System.out.println("My name is " + name);
+    }
+    public void printAge() {
+        System.out.println("My age is " + age);
+
     }
 }
