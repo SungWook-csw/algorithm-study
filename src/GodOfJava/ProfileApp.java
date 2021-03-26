63fa56e2d2e540838db88ecfd39a448e6cccfe01
SungWook-csw <choisw950928@naver.com>
2021-03-27 01:13:55 +0900

    feat : GodOfJava 1nd Chap.3

---
+++ src/GodOfJava/ProfileApp.java
@@ -0,0 +1,11 @@
+package GodOfJava;
+
+public class ProfileApp {
+    public static void main(String[] args) {
+        String name = "최성욱";
+        int age = 27;
+
+        System.out.println("My name is" + "최성욱");
+        System.out.println("My is age" + 27);
+    }
+}
