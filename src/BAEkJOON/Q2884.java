cf7fbf5129e14f567be5799c136361d8a863cc1c
SungWook-csw <choisw950928@naver.com>
2021-03-29 00:37:38 +0900

    feat : 백준 Q2884 알람시계

---
+++ src/BAEkJOON/Q2884.java
@@ -0,0 +1,25 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q2884 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int Hour = scan.nextInt();
+        int Minute = scan.nextInt();
+
+        if (Minute < 45) {
+            Hour--;
+            Minute = 60 - (45 - Minute);
+
+            if (Hour < 0) {
+                Hour = 23;
+            }
+            System.out.println(Hour + "" + Minute);
+        } else {
+            System.out.println(Hour + "" + (Minute - 45));
+        }
+
+    }
+}
