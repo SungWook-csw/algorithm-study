af1a37b1c667510916218b0e6b040779eb6b7b87
SungWook-csw <choisw950928@naver.com>
2021-03-30 22:39:47 +0900

    feat : Method 의 기본 형식1

---
+++ src/LifeCoding/FirstMethod.java
@@ -0,0 +1,9 @@
+package LifeCoding;
+
+public class FirstMethod {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Method"); // Println은 입력값을 출력하는 메소드
+        System.out.println(Math.floor(1.1));
+    }
+}
