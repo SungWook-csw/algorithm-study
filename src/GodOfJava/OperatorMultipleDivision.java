5ff199bf5924da09ee20cbafaa569161a5292782
SungWook-csw <choisw950928@naver.com>
2021-04-07 23:43:16 +0900

    feat : GodofJava 1nd 5장 산술연산자 예제

--- src/GodOfJava/OperatorMultipleDivision.java
+++ src/GodOfJava/OperatorMultipleDivision.java
@@ -3,16 +3,28 @@ package GodOfJava;
 public class OperatorMultipleDivision {
     public static void main(String[] args) {
         OperatorMultipleDivision sample = new OperatorMultipleDivision();
-        sample.MultipleDivision();
+        sample.divisionInt2();
     }
 
-    public void MultipleDivision() {
+/*    public void MultipleDivision() { // 곱하기 / 나누기
         int intValue1 = 5;
         int intValue2 = 10;
 
         int result = intValue1 * intValue2;
         System.out.println(result);
-        result = intValue2 / intValue1;
+        System.out.println(result);
+        result = intValue2 / intValue1; */
+
+/*    public void divisionInt() {  //나누기 결과 값 0 일 때
+        int intValue1 = 5;
+        int intValue2 = 10;
+        int result = intValue1 / intValue2;
+        System.out.println(result); */
+
+    public void divisionInt2(){ // 나누기 결과를 0.5인 실수로 바꿀때 
+        double doubleValue1=5;
+        double doubleValue2=10;
+        double result=doubleValue1/doubleValue2;
         System.out.println(result);
     }
 }
