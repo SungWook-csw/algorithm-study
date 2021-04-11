1e26533c7bf2e3ea2638c34b2680a290eb253d03
SungWook-csw <choisw950928@naver.com>
2021-04-11 21:08:10 +0900

    feat : 백준 Q2438 별찍기

---
+++ src/BAEkJOON/Q2438.java
@@ -0,0 +1,19 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q2438 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
