42c71858154e14e9ca6b74171482d5e93f21b517
SungWook-csw <choisw950928@naver.com>
2021-04-14 22:39:14 +0900

    feat : 자바의 신 6장 if문 연습

---
+++ src/GodOfJava/Controlif.java
@@ -0,0 +1,17 @@
+package GodOfJava;
+
+public class Controlif {
+    public static void main(String[] args) {
+        Controlif controlif = new Controlif();
+        controlif.ifStatement();
+
+    }
+
+        public void ifStatement() {
+            if (true);
+            if (true) System.out.println("It's ture");
+            if (true)
+                System.out.println("It's also true.");
+            if (false) System.out.println("It's a false");
+        }
+    }
