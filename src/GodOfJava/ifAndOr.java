df401b39443ca62f28e2695f6ef1f55de3aac8c8
SungWook-csw <choisw950928@naver.com>
2021-04-14 22:55:15 +0900

    feat : 자바의 신 6장 tripleOrAnd

--- src/GodOfJava/ifAndOr.java
+++ src/GodOfJava/ifAndOr.java
@@ -3,18 +3,30 @@ package GodOfJava;
 public class ifAndOr {
     public static void main(String[] args) {
         ifAndOr scan = new ifAndOr();
-        scan.ifAndor();
+        scan.tripleOrAnd();
     }
 
-    public void ifAndor() {
+    /*    public void ifAndor() {
+            int age = 25;
+            boolean isMrried = true;
+
+            if (age > 25 && isMrried) {
+                System.out.println("Age is over 25 and Married");
+            }
+            if (age > 25 || isMrried) {
+                System.out.println("Age is over 25 or Married");
+            }
+        }
+    } */
+    public void tripleOrAnd() {
         int age = 25;
-        boolean isMrried = true;
-
-        if (age > 25 && isMrried) {
-            System.out.println("Age is over 25 and Married");
+        boolean isMarried = true;
+        double height = 180;
+        if ((age > 25 || isMarried) && height >= 180) {
+            System.out.println("Age is over 25 or Married and height is over 180");
         }
-        if (age > 25 || isMrried) {
-            System.out.println("Age is over 25 or Married");
         }
     }
-}
+
+
+
