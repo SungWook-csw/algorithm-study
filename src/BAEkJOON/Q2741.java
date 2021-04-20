e5bd3d40ca9c35deda586c95b33738345d56a50a
SungWook-csw <choisw950928@naver.com>
2021-04-20 23:20:07 +0900

    feat : 백준 Q2741 N찍기 확인

--- src/BAEkJOON/Q2741.java
+++ src/BAEkJOON/Q2741.java
@@ -2,15 +2,14 @@ package BAEkJOON;
 
 import java.util.Scanner;
 
-public class Q2739 {
+public class Q2741 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int N = scan.nextInt();
 
-        for (int i = 1; i <= 9; ++i) { // 종료 조건 : 9까지는 반복해라
-            System.out.println(N + " * " + i + " = " + N * i); // 공백 주의
+        for (int i = 1; i <= N ; i++) {
+            System.out.println(i);
         }
     }
 }
-
