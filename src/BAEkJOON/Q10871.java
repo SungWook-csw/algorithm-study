13c089bb128cf51566173d05fd0fb51400a84003
SungWook-csw <choisw950928@naver.com>
2021-04-22 00:13:56 +0900

    feat : Q10871 추후 다시 진행 예정

---
+++ src/BAEkJOON/Q10871.java
@@ -0,0 +1,19 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q10871 {  // 못품
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int x = scan.nextInt();
+
+        for (int i = 0; i <n; i++) {
+            int num = scan.nextInt();
+            if (num < x) {
+                System.out.println(num + " ");
+            }
+        }
+    }
+}
