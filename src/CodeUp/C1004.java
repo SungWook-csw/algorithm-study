c3fa6651350f88ff9f6776ae91775028620a9ddd
SungWook-csw <choisw950928@naver.com>
2021-04-22 00:40:00 +0900

    feat : CodeUp 1004

---
+++ src/CodeUp/C1004.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+public class C1004 {
+        public static void main(String[] args) {
+            System.out.println("\'Hello\'"); // \' 는 작은따옴표 문자 자체 출력을 한다.
+        }
+    }
+
+// \' -> 작은따옴표 문자 자체를 출력
+// \" -> 큰따옴표 문자 자체를 출력
+// 따옴표 자체를 표현해 주기 위한 행동
