0f9ccee2321b5433016772ba93495607afc09ee9
SungWook-csw <choisw950928@naver.com>
2021-04-21 23:31:04 +0900

    feat : If 조건문 기초

---
+++ src/LifeCoding/IfApp1.java
@@ -0,0 +1,12 @@
+package LifeCoding;
+
+public class IfApp1 {
+    public static void main(String[] args) {
+
+        if (false) { // if 값이 참이면 1을 출력, false 라면 2를 출력
+            System.out.println(1);
+        }else {
+            System.out.println(2);
+        }
+    }
+}
