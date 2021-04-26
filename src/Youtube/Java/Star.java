53d7352fecdb0c4e142d892d22c1b598e73b76b9
SungWook-csw <choisw950928@naver.com>
2021-04-26 23:05:31 +0900

    feat : 별찍기 기초

---
+++ src/Youtube/Java/Star.java
@@ -0,0 +1,14 @@
+package Youtube.Java;
+
+public class Star {
+    public static void main(String[] args) {
+
+        // i = 1, 2, 3, 4, 5 번의 {} 내용을 반복
+        for (int i = 0; i < 5 ; i++) {
+            for (int j = 0; j < 10 ; j++) {
+                System.out.print("*");
+            }
+            System.out.println(); // 줄바꿈
+        }
+    }
+}
