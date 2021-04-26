8d1a94ab9f8267f9830c599a1c7875ac7ea7e50e
SungWook-csw <choisw950928@naver.com>
2021-04-27 01:06:47 +0900

    feat : Q2523  별찍기 13

---
+++ src/BAEkJOON/Q2523.java
@@ -0,0 +1,25 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q2523 {
+    public static void main (String [] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1 ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for (int i = 1; i < n ; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
