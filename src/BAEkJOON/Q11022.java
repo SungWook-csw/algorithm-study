5276086a98a95b15ab7498b4ef4b8a3885e0bd91
SungWook-csw <choisw950928@naver.com>
2021-04-29 21:53:04 +0900

    feat : Q11022 A+B-8

---
+++ src/BAEkJOON/Q11022.java
@@ -0,0 +1,20 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q11022 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int T = scan.nextInt();
+
+        for (int i = 1; i <= T; i++) {
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+            int C = A + B;
+
+            System.out.println("Case #" + i + ": " + A + " + " + B + " = " + C);
+
+        }
+    }
+}
