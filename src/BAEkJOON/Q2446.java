415ef52efce51e3712d474d53b4239f1e1edf6c7
SungWook-csw <choisw950928@naver.com>
2021-05-27 23:20:02 +0900

    배열 과 for문 조화

---
+++ src/BAEkJOON/Q2446.java
@@ -0,0 +1,31 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q2446 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = N; i >= 0; i--) {
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+            for (int j = N; j >= i; j--) {
+                System.out.print(" ");
+            }
+        }
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < N- i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
