9ae21e8aeb55386d4aade7dd4626c931b23864cd
SungWook-csw <choisw950928@naver.com>
2021-05-30 17:23:10 +0900

    Q10951 A+B -4 틀림

---
+++ src/BAEkJOON/Q10951.java
@@ -0,0 +1,18 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q10951 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int 저장소 = (4);
+
+        for (int i = 0; i <= 저장소 ; i++) {
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+
+            System.out.println(A + B);
+        }
+    }
+}
