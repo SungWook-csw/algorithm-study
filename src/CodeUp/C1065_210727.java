2054ddaf1aa134de19d56c0d119b866b9e132a71
SungWook-csw <choisw950928@naver.com>
2021-07-27 00:24:39 +0900

    C1065_210727 if문 기초 예제

---
+++ src/CodeUp/C1065_210727.java
@@ -0,0 +1,20 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1065_210727 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+
+        if (A % 2 == 0) {
+            System.out.println(A);
+        } if (B % 2 == 0) {
+            System.out.println(B);
+        } if (C % 2 == 0) {
+            System.out.println(C);
+        }
+    }
+}
