7e601ae2bad265d6e02db4250c7f2ead4566ad8c
SungWook-csw <choisw950928@naver.com>
2021-07-29 00:52:39 +0900

    C1154_210729 큰수 작은수 minus 값 출력하기

---
+++ src/CodeUp/C1154_210729.java
@@ -0,0 +1,18 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1154_210729 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 - num2);
+        } else {
+            System.out.println(num2 - num1);
+        }
+    }
+}
