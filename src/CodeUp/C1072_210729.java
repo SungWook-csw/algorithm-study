31fc68273ed8fb6b20fce941e87d26f207a3a1e2
SungWook-csw <choisw950928@naver.com>
2021-07-29 23:39:55 +0900

    C1072_210729 for문 이용하여 정수 입력 받아 출력하기 (틀림)

---
+++ src/CodeUp/C1072_210729.java
@@ -0,0 +1,16 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1072_210729 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for (int i = 1; i <= n ; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
