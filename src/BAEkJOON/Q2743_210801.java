ff3356284a4c061d87b1f1c8a3790f4c814eabaf
SungWook-csw <choisw950928@naver.com>
2021-08-01 20:19:31 +0900

    Q2743_210801 글자수 출력하기

---
+++ src/BAEkJOON/Q2743_210801.java
@@ -0,0 +1,14 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q2743_210801 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String N = scan.nextLine();
+
+        System.out.println(N.length());
+
+    }
+}
