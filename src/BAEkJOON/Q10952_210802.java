e2e73d0973d25545c32d5014aadcb7a9423f3fa4
SungWook-csw <choisw950928@naver.com>
2021-08-02 10:46:57 +0900

    Q10952_210802 무한반복 while문 이용하여 0과 0이 나오면 브레이크

---
+++ src/BAEkJOON/Q10952_210802.java
@@ -0,0 +1,21 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q10952_210802 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            int A = scan.nextInt();
+            int B = scan.nextInt();
+
+            if (A == 0 && B == 0) {
+                break;
+            } else {
+                System.out.println(A + B);
+            }
+        }
+    }
+}
+
