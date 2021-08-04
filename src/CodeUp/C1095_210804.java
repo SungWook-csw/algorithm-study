aeb3b2a0109fa12c140016597f55949fcf706b85
SungWook-csw <choisw950928@naver.com>
2021-08-04 16:12:01 +0900

    C1095_210804 배열과 포문을 사용 가장 빠른 번호 1개만 출력하는 문제 - Arrays.sort 사용하여 오름차순으로 정렬해준다. - 출력을 첫번째 칸인 arr[0] 으로 출력해준다.

---
+++ src/CodeUp/C1095_210804.java
@@ -0,0 +1,19 @@
+package CodeUp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class C1095_210804 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0]);
+    }
+}
