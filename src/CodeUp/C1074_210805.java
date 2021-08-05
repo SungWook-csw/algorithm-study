a5e733d91445c81a0314ddbd9b83b721bc255ea9
SungWook-csw <choisw950928@naver.com>
2021-08-05 11:35:50 +0900

    C1074_210805 <정수 입력 받아 카운트 다운 출력> - N개의 입력 - N 개만큼 for문 활용

---
+++ src/CodeUp/C1074_210805.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1074_210805 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(N-i);
+        }
+    }
+}
