766fc91de0661b1f6e4f309231a3923e7a89a5ff
SungWook-csw <choisw950928@naver.com>
2021-08-05 18:28:01 +0900

    C1160_210805 <아르바이트 가는 날 출력하는 문제>

---
+++ src/CodeUp/C1160_210805.java
@@ -0,0 +1,23 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1160_210805 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        if (N == 1) {
+            System.out.println("oh my god");
+        } else if (N == 3) {
+            System.out.println("oh my god");
+        } else if (N == 5) {
+            System.out.println("oh my god");
+        } else if (N == 7) {
+            System.out.println("oh my god");
+        } else {
+            System.out.println("enjoy");
+        }
+    }
+}
