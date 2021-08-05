b687570e3b58e7b5536d0d159d0b2fa1276cc19c
SungWook-csw <choisw950928@naver.com>
2021-08-05 16:02:45 +0900

    C1010_210805 <문자 1개 입력받아 그대로 출력하기> - char N = scan.next().charAt(0)

--- src/CodeUp/C1010_210805.java
+++ src/CodeUp/C1010_210805.java
@@ -6,7 +6,7 @@ public class C1010_210805 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int N = scan.nextInt();
+        char N = scan.next().charAt(0);
         System.out.println(N);
     }
 }
