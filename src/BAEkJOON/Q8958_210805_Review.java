e1235627de07f5f688da748366950095a184af03
SungWook-csw <choisw950928@naver.com>
2021-08-05 14:03:49 +0900

    Q8958_210805_Review <O,X퀴즈>

---
+++ src/BAEkJOON/Q8958_210805_Review.java
@@ -0,0 +1,30 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q8958_210805_Review {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            String 문자 = scan.next();
+            char[] arr = new char[문자.length()];
+
+            int count = 1;
+            int value = 0;
+
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = 문자.charAt(j);
+                if (arr[j] == 'X') {
+                    count = 1;
+                    continue;
+                }
+                value = value + count ++;
+            }
+            System.out.println(value);
+        }
+    }
+}
+
