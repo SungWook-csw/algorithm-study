01e7006fb5142ea4312c75e317862509d93b52e8
SungWook-csw <choisw950928@naver.com>
2021-08-05 16:38:24 +0900

    C1012_210805_float <실수 1개를 입력 받아 그대로 출력하기> - println 은 변수의 값을 그대로 출력 - printf 는 변수의 값을 변환하여 출력

---
+++ src/CodeUp/C1012_210805_float.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1012_210805_float {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        float n = scan.nextFloat();
+        System.out.printf("%f", n);
+    }
+}
