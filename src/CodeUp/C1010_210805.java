3a6565b7dbe11bdbd39fb5dfe66432fe50223e2d
SungWook-csw <choisw950928@naver.com>
2021-08-05 15:50:17 +0900

    C1010_210805 <정수 1개 입력받아 그대로 출력>

---
+++ src/CodeUp/C1010_210805.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1010_210805 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        System.out.println(N);
+    }
+}
