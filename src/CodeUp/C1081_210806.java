1a78413ad68d74682d1fb6fe71dda6bfebbece69
SungWook-csw <choisw950928@naver.com>
2021-08-06 10:11:30 +0900

    C1081_210806 <주사위를 2개 던졌을 때 나올 수 있는 경우 의 수> - 중첩 for문 사용 - 2개 입력 - 저장공간 사용 - i 와 j 를 이용하여 순차적으로 뽑아내기

---
+++ src/CodeUp/C1081_210806.java
@@ -0,0 +1,24 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1081_210806 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+
+        int storage = 0;
+        int storage1 = 0;
+
+        for (int i = 1; i <= num1; i++) {
+            for (int j = 0; j < num2; j++) {
+                storage = i;
+                storage1 = j+1;
+
+                System.out.println(storage + " " +storage1);
+            }
+        }
+    }
+}
