4ff2aebe398d84640573dd37049afe2ef6bb2b3e
SungWook-csw <choisw950928@naver.com>
2021-08-06 10:17:52 +0900

    C1077_210806 <단순 반복문 정수 1개 입력 받아 순차적으로 출력>

---
+++ src/CodeUp/C1077_210806.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1077_210806 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+
+        for (int i = 0; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
