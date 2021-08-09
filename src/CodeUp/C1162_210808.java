644a339029b6327f77ecc424132084607a29b442
SungWook-csw <choisw950928@naver.com>
2021-08-09 21:36:59 +0900

    C1162_210808 <당신의 사주를 봐 드립니다1>

--- src/CodeUp/C1162_210808.java
+++ src/CodeUp/C1162_210808.java
@@ -6,23 +6,17 @@ public class C1162_210808 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int Year = scan.nextInt();
-        int Month = scan.nextInt();
-        int Day = scan.nextInt();
+        int Y = scan.nextInt();
+        int M = scan.nextInt();
+        int D = scan.nextInt();
 
-        while (true) {
-            int sum = 0;
+        int value = Y - M + D;
+        int num = value % 10;
 
-            if (Year > 0 && Month > 0 && Day > 0) {
-                sum = Year - Month + Day;
-            }
-            if (sum % 10 == 0) {
-                System.out.println("대박");
-                break;
-            } else {
-                System.out.println("그럭저럭");
-                break;
-            }
+        if (num % 10 == 0) {
+            System.out.println("대박");
+        }else {
+            System.out.println("그럭저럭");
         }
     }
 }
