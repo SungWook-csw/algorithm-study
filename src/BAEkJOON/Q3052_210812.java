b058e6a87049a6de02cc499531a27ea9e34467d2
SungWook-csw <choisw950928@naver.com>
2021-08-12 23:50:05 +0900

    Q3052_210812 다시 <나머지> - 자연수 10개를 입력 후 입력한 값에 대해 42로 나눈 나머지 값 중 서로 다른 값의 카운트 세는 문제 1. 42로 나는 나머지 값들을 배열에 저장한다. 2. 현재 값과 나머지 값들을 비교하여 같은 경우 cnt를 1씩 더해준다. 3. cnt 값이 0이라면 다른값과 겹치지 않음을 알수있다.

---
+++ src/BAEkJOON/Q3052_210812.java
@@ -0,0 +1,28 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q3052_210812 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] arr = new int[10];
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt() % 42;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int cnt = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    cnt++;
+                }
+            }
+            if (cnt == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
