7dda382223a259feaa8ee94559ce8cc797a0558d
SungWook-csw <choisw950928@naver.com>
2021-08-21 15:47:29 +0900

    Q3046_210821 <R2>

---
+++ src/BAEkJOON/Q3046_210821.java
@@ -0,0 +1,18 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q3046_210821 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        int R1 = scan.nextInt();
+        int 평균 = scan.nextInt() * 2;
+        int R2 = 0;
+
+        R2 = 평균 - R1;
+
+        System.out.println(R2);
+    }
+}
