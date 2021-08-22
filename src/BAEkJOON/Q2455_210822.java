ecd4a6505d2557d51268595f95bd8167498a8f34
SungWook-csw <choisw950928@naver.com>
2021-08-22 13:56:21 +0900

    Q2455_210822 <지능형 기차> - 제일 많이 타고 있는 역의 사람수 출력하기 1. in out을 반복하여 입력해주기 위해 for문 안에 입력 2. sum이라는 저장소를 만들어서 in 했을때 값을 저장 다시, sum 이라는 저장소에서 - out을한다. 3. 조건을 주어 max라는 저장소 보다 sum 이 더 클 경우 max값에 저장

---
+++ src/BAEkJOON/Q2455_210822.java
@@ -0,0 +1,25 @@
+package BAEkJOON;
+
+import java.util.Scanner;
+
+public class Q2455_210822 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < 4; i++) {
+            int out = scan.nextInt();
+            int in = scan.nextInt();
+
+            sum = sum + in;
+            sum = sum - out;
+
+            if (max < sum) {
+                max = sum;
+            }
+        }
+        System.out.println(max);
+    }
+}
