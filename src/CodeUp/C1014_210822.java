d168a2d3d06c76cdca4add22408ad0b5245cf89b
SungWook-csw <choisw950928@naver.com>
2021-08-22 12:24:59 +0900

    C1014_210822 <문자 2개 입력 받아 순서 바꿔 출력하기>
    -String 입력 방법 기억!!

---
+++ src/CodeUp/C1014_210822.java
@@ -0,0 +1,14 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1014_210822 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String A = scan.next();
+        String b = scan.next();
+
+        System.out.println(b + " " + A);
+    }
+}
