68da719c49a42bb6543a50746d99c3cae9ad3b76
SungWook-csw <choisw950928@naver.com>
2021-08-28 13:14:40 +0900

    C1015_210828 <실수 입력받아 둘째 자리까지 출력하기>
    1. 입력에 실수(float) 입력을 받는다.
    float N = scan.nextFloat();
    2. 소수점 둘째자리까지 출력을 위해
    String.format을 사용한다.
    %.2f와 같은 형식으로 지정하면 소수점 이하 셋째 자리에서 반올림하여 소수점 이하 둘째 자리까지 출력한다.

---
+++ src/CodeUp/C1015_210828.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1015_210828 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        float N = scan.nextFloat();
+
+        System.out.println(String.format("%.2f",N));
+    }
+}
