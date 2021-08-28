9b9ce1cd591efe3c4cacea07f75b34197c3b26d9
SungWook-csw <choisw950928@naver.com>
2021-08-28 13:25:21 +0900

    C1017_210828 <정수 1개 입력받아 3번 출력하기>
    - int d = scan.nextInt();
    - String.format("%d %d %d", d,d,d)
    -> "%d %d %d" 문자열안에 d를 각각 대입한다라고 생각하면 된다.

---
+++ src/CodeUp/C1017_210828.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1017_210828 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int d = scan.nextInt();
+
+        System.out.println(String.format("%d %d %d", d, d, d));
+    }
+}
