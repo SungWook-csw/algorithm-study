a1cfac0cd407acf2533f79ddcd8b43069c6c4adb
SungWook-csw <choisw950928@naver.com>
2021-08-28 14:06:55 +0900

    C1018_210828 <시간 입력받아 그대로 출력하기> - split을 사용하여 : 기준으로 쪼개어 준다. ※split 메소드에는 문자열 배을가지고 있다※ - 출력에 num[0] + ":" + num[1] 을 출력

---
+++ src/CodeUp/C1018_210828.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1018_210828 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String num[] = scan.next().split(":");
+
+        System.out.println(num[0] + ":" + num[1]);
+    }
+}
