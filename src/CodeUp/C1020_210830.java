86eece1cae9f01ce8060811d27e65bf65fcb19fa
SungWook-csw <choisw950928@naver.com>
2021-08-30 23:18:29 +0900

    C1020_210830 <주민번호 입력 형태 바꾸어 출력하기> -replace 사용 (치환한다는 뜻으로 (기존문자,바꿀문자); 입력

---
+++ src/CodeUp/C1020_210830.java
@@ -0,0 +1,14 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1020_210830 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        str = str.replace("-","");
+
+        System.out.println(str);
+    }
+}
