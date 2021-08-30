a136ec5089b862c827127ce70afbc86b37ffc95d
SungWook-csw <choisw950928@naver.com>
2021-08-31 00:13:45 +0900

    ex_210831 Math.random 무작위로 숫자 출력 0.0 ~ 0.99999... 1.0까지 double 형으로 반환 1. 정수로 바꾸려면 int로 캐스팅 2. *(곱하기) 값이 내가 뽑아내길 원하는 값

---
+++ src/Example/ex_210831.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class ex_210831 {
+    public static void main(String[] args) {
+        int num = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            num = ((int) (Math.random() * 6) + 1);
+            System.out.println(num);
+        }
+    }
+}
