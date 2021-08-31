634d0a19f5c3fd4521ba7b46464ec816aa583fe2
SungWook-csw <choisw950928@naver.com>
2021-08-31 22:41:38 +0900

    C1020_210831_Review replac 치환 함수 적용

---
+++ src/CodeUp/C1020_210831_Review.java
@@ -0,0 +1,14 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1020_210831_Review {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        str = str.replace("-","");
+
+        System.out.println(str);
+    }
+}
