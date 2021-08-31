c7433ed076c889ed5e6023fe6f7f65096d7e2779
SungWook-csw <choisw950928@naver.com>
2021-09-01 00:05:56 +0900

    배열 연습

---
+++ src/Example/ex_Ex5_210831.java
@@ -0,0 +1,20 @@
+package Example;
+
+public class ex_Ex5_210831 {
+    public static void main(String[] args) {
+//        int[] arr; // 배열을 선언
+//        arr = new int[5]; // 배열의 생성
+
+        int[] arr = new int[5]; // 배열이 선언과 생성
+        arr[3] = 100;
+
+        System.out.println("score[0]=" + arr[0]);
+        System.out.println("score[1]=" + arr[1]);
+        System.out.println("score[2]=" + arr[2]);
+        System.out.println("score[3]=" + arr[3]);
+        System.out.println("score[4]=" + arr[4]);
+
+        int value = 100;
+        System.out.println("value=" + value);
+    }
+}
