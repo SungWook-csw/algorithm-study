088d2d336e2013578b3d5d6ff321e9d44dead918
SungWook-csw <choisw950928@naver.com>
2021-08-31 23:00:02 +0900

    while 연습

---
+++ src/Example/ex_loop_210831.java
@@ -0,0 +1,12 @@
+package Example;
+
+public class ex_loop_210831 {
+    public static void main(String[] args) {
+        int i = 5; //초기화
+
+        while (i != 0) { // 조건식
+            i--; // 증감식
+            System.out.println(i + "-I can do it.");
+        }
+    }
+}
