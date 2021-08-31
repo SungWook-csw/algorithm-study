3ab26fd8283b91738f4fa16e1b2cb61360bf85b4
SungWook-csw <choisw950928@naver.com>
2021-08-31 22:53:11 +0900

    C1021_210831 <단어 1개 입력받아 그대로 출력하기> Easy

---
+++ src/CodeUp/C1021_210831.java
@@ -0,0 +1,12 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1021_210831 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+        System.out.println(str);
+    }
+}
