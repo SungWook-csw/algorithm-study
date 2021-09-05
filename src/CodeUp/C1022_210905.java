32c9b7f8cad8a3abf7d297ab61fee9c3ddc724e7
SungWook-csw <choisw950928@naver.com>
2021-09-05 14:23:54 +0900

    C1022_210905 문장 1개 입력 받아 그래로 출력하기 - scan.nextLine() 활용하기 한 행을 출력

---
+++ src/CodeUp/C1022_210905.java
@@ -0,0 +1,13 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1022_210905 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+
+        System.out.println(str);
+    }
+}
