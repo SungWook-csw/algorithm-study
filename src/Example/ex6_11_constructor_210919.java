ca2afd45ced8c674e9e84b1abb655c224dd0649a
SungWook-csw <choisw950928@naver.com>
2021-09-19 14:43:01 +0900

    ex6_11_constructor_210919 기본생성자

---
+++ src/Example/ex6_11_constructor_210919.java
@@ -0,0 +1,21 @@
+package Example;
+
+class Data_1 {
+    int value;
+}
+
+class Data_2 {
+    int value;
+
+    Data_2() {} // 기본 생성자
+    Data_2(int x) { // 매개변수가 있는 생성자.
+        value = x;
+    }
+}
+
+class ex6_11_constructor_210919 {
+    public static void main(String[] args) {
+        Data_1 d1 = new Data_1();
+        Data_2 d2 = new Data_2();
+    }
+}
